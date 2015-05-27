5ca651dbbe46f4a3992570ea34dd6774d07acaa7
vgonsalez <victorgonsalez@mail.ru>
2015-05-27 14:28:01 -0300

    First the commit

---
+++ mobile/src/main/java/com/example/vgonsalez/sampleweargoogleio/MainActivity.java
@@ -0,0 +1,62 @@
+package com.example.vgonsalez.sampleweargoogleio;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.support.v4.app.NotificationCompat;
+import android.support.v4.app.NotificationManagerCompat;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+
+
+public class MainActivity extends Activity {
+private Button mBtnNotification;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        mBtnNotification = (Button) findViewById(R.id.btn_send_notification);
+        mBtnNotification.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                int notificationId = Integer.valueOf(1);
+                // Build intent for notification content
+
+                NotificationCompat.WearableExtender wearableExtender = new NotificationCompat.WearableExtender();
+                NotificationCompat.Builder notificationBuilder = new NotificationCompat.Builder(MainActivity.this)
+                        .setSmallIcon(R.drawable.compass67)
+                        .setContentTitle(getResources().getString(R.string.title_notification))
+                        .setContentText(getResources().getString(R.string.context_notification)).extend(wearableExtender);
+
+                NotificationManagerCompat notificationManager2 = NotificationManagerCompat.from(MainActivity.this);
+                // Build the notification and issues it with notification manager.
+                notificationManager2.notify(notificationId, notificationBuilder.build());
+            }
+        });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+
+}
