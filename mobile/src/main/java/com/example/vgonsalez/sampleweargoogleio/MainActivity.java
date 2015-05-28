028fd3618218f90cd0caa9fa82c9223c42eca857
DeKoServidoni <deko.puc@gmail.com>
2015-05-27 23:08:07 -0300

    Added new layout

--- mobile/src/main/java/com/example/vgonsalez/sampleweargoogleio/MainActivity.java
+++ mobile/src/main/java/com/example/vgonsalez/sampleweargoogleio/MainActivity.java
@@ -4,19 +4,21 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.support.v4.app.NotificationCompat;
 import android.support.v4.app.NotificationManagerCompat;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 
 public class MainActivity extends Activity {
-    private Button mBtnNotification;
+
+    private TextView mTxResponse = null;
+    private Button mBtnNotification = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
         mBtnNotification = (Button) findViewById(R.id.btn_send_notification);
         mBtnNotification.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -24,28 +26,8 @@ public class MainActivity extends Activity {
                 sendNotification();
             }
         });
-    }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
+        mTxResponse = (TextView) findViewById(R.id.main_response);
     }
 
     private void sendNotification() {
