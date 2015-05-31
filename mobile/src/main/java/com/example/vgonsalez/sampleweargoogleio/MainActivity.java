8ecfadd3db6d9c94ea9a1c561a22d95befbe9ae1
victorgonsalez <victorgonsalez@mail.ru>
2015-05-31 15:02:10 -0300

    Updating for a new version

--- mobile/src/main/java/com/example/vgonsalez/sampleweargoogleio/MainActivity.java
+++ mobile/src/main/java/com/example/vgonsalez/sampleweargoogleio/MainActivity.java
@@ -1,18 +1,27 @@
 package com.example.vgonsalez.sampleweargoogleio;
 
 import android.app.Activity;
+import android.app.PendingIntent;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
 import android.os.Bundle;
-import android.support.v4.app.NotificationCompat;
+import android.support.v4.app.NotificationCompat.Action;
+import android.support.v4.app.NotificationCompat.Builder;
+import android.support.v4.app.NotificationCompat.WearableExtender;
 import android.support.v4.app.NotificationManagerCompat;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
-
 
 public class MainActivity extends Activity {
 
-    private TextView mTxResponse = null;
-    private Button mBtnNotification = null;
+    private EditText mEditTitle;
+    private EditText mEditContext;
+    private EditText mEditData;
+    private Button mBtnNotification;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,16 +36,38 @@ public class MainActivity extends Activity {
             }
         });
 
-        mTxResponse = (TextView) findViewById(R.id.main_response);
+        mEditTitle = (EditText) findViewById(R.id.title_notification);
+        mEditContext = (EditText) findViewById(R.id.context_notification);
+        mEditData = (EditText) findViewById(R.id.data_notification);
     }
 
     private void sendNotification() {
+
+        Intent mapIntent = new Intent(Intent.ACTION_VIEW);
+        Uri geoUri = Uri.parse("geo:0,0?q=" + Uri.encode(mEditData.getText().toString()));
+        mapIntent.setData(geoUri);
+        PendingIntent mapPendingIntent = PendingIntent.getActivity(this, 0, mapIntent, 0);
+
+        Intent videoIntent = new Intent(Intent.ACTION_VIEW);
+        Uri videoUri = Uri.parse("https://www.youtube.com/results?search_query="+mEditData.getText().toString());
+        videoIntent.setData(videoUri);
+        PendingIntent videoPendingIntent = PendingIntent.getActivity(this, 0, videoIntent, 0);
+
+        Action mapAction = new Action.Builder(R.drawable.map,
+                getString(R.string.map_action), mapPendingIntent).build();
+        Action videoAction = new Action.Builder(R.drawable.youtube,
+                getString(R.string.video_action), videoPendingIntent).build();
+
+        Bitmap bitmap = BitmapFactory.decodeResource(mEditContext.getResources(),R.drawable.android_background);
         int notificationId = 1;
-        NotificationCompat.WearableExtender wearableExtender = new NotificationCompat.WearableExtender();
-        NotificationCompat.Builder notificationBuilder = new NotificationCompat.Builder(MainActivity.this)
-                .setSmallIcon(R.drawable.compass67)
-                .setContentTitle(getResources().getString(R.string.title_notification))
-                .setContentText(getResources().getString(R.string.context_notification)).extend(wearableExtender);
+        Builder notificationBuilder = new Builder(MainActivity.this)
+                .setSmallIcon(R.drawable.compass)
+                .setLargeIcon(bitmap)
+                .setContentTitle(mEditTitle.getText().toString())
+                .setContentText(mEditContext.getText().toString())
+                .extend(new WearableExtender()
+                        .addAction(mapAction)
+                        .addAction(videoAction));
 
         NotificationManagerCompat notificationManager = NotificationManagerCompat.from(MainActivity.this);
         notificationManager.notify(notificationId, notificationBuilder.build());
